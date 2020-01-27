cbeac566793f8323d0b88d738303ce4a5b236999
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-27 15:56:39 -0600

    CAP-309 src/cap-angular-schematic-auth-firebase/index.ts Implement the installation of packages necessary for the module and also import the firebase module in the app.module.ts of the target project.

--- src/cap-angular-schematic-auth-firebase/index.ts
+++ src/cap-angular-schematic-auth-firebase/index.ts
@@ -1,6 +1,48 @@
-import { apply, FileEntry, forEach, MergeStrategy, mergeWith, move, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
+import {
+  apply,
+  FileEntry,
+  forEach,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url,
+  chain,
+  noop,
+  SchematicsException
+} from '@angular-devkit/schematics';
+
+import {
+  addPackageJsonDependency,
+  getWorkspace,
+  NodeDependency,
+  NodeDependencyType,
+  getProjectFromWorkspace,
+  getAppModulePath,
+  WorkspaceProject,
+  addImportToModule,
+  // findPropertyInAstObject,
+  // findModule,
+  // removePropertyInAstObject,
+  // InsertChange,
+} from 'schematics-utilities';
 import { join, normalize } from 'path';
-import { getWorkspace } from '@schematics/angular/utility/config';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { getProjectMainFile, getSourceFile } from 'schematics-utilities/dist/cdk';
+
+
+export default function (options: any): Rule {
+  return chain([
+    options && options.skipModuleImport ? noop() : capAngularSchematicAuthFirebase(options),
+    options && options.skipPackageJson ? noop() : addPackageJsonDependencies(),
+    options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
+    options && options.skipModuleImport ? noop() : addModuleToImports(options),
+  ]);
+}
+
 
 export function setupOptions(host: Tree, options: any): Tree {
   const workspace = getWorkspace(host);
@@ -9,7 +51,7 @@ export function setupOptions(host: Tree, options: any): Tree {
   }
   const project = workspace.projects[options.project];
 
-  options.path = join(normalize(project.root), 'src/app/modules/cap-auth-firebase');
+  options.path = join(normalize(project.root), 'src/app/modules/cap-auth');
   return host;
 }
 
@@ -35,3 +77,71 @@ export function capAngularSchematicAuthFirebase(_options: any): Rule {
     return rule(tree, _context);;
   };
 }
+
+export function addPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const dependencies: NodeDependency[] = [
+      { type: NodeDependencyType.Default, version: '^7.2.3', name: 'firebase' },
+      { type: NodeDependencyType.Default, version: '^5.2.1', name: '@angular/fire' },
+      { type: NodeDependencyType.Default, version: '^4.3.1', name: 'bootstrap' },
+    ];
+    dependencies.forEach(dependency => {
+      addPackageJsonDependency(host, dependency);
+      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+    });
+    return host;
+  };
+}
+
+export function installPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    context.logger.log('info', `🔍 Installing packages...`);
+    return host;
+  };
+}
+
+function addModuleToImports (options: any): Rule {
+  return (host: Tree) => {
+    const workspace = getWorkspace(host);
+    let project : WorkspaceProject = getProjectFromWorkspace(workspace, options.project);
+    const moduleName = 'CapAuthModule';
+    const modulePath = getAppModulePath(host, getProjectMainFile(project));
+    auxAddModuleRoorToImports(host, modulePath, moduleName, './modules/cap-auth/cap-auth.module', options);
+    auxAddModuleRoorToImports(host, modulePath, 'AngularFireAuthModule', '@angular/fire/auth', options);
+    auxAddModuleRoorToImports(host, modulePath, 'AngularFireModule', '@angular/fire', options);
+    return host;
+  };
+}
+
+export function auxAddModuleRoorToImports (host: Tree, modulePath: string, moduleName: string, src: string, options?: any) {
+  const moduleSource = getSourceFile(host, modulePath);
+  if (!moduleSource) {
+    throw new SchematicsException(`Module not found: ${modulePath}`);
+  }
+
+  const changes = addImportToModule(moduleSource as any, modulePath, moduleName, src);
+  let recorder = host.beginUpdate(modulePath);
+  changes.forEach((change:any) => {
+    // if (change instanceof InsertChange) {
+      if (change.toAdd) {
+        if (change.toAdd === ',\n    AngularFireModule') {
+          change.toAdd = `,\n    AngularFireModule.initializeApp({
+      apiKey: '${options.apiKey}',
+      authDomain: '${options.authDomain}',
+      databaseURL: '${options.databaseURL}',
+      projectId: '${options.projectId}',
+      storageBucket: '${options.storageBucket}',
+      messagingSenderId: '${options.senderId}',
+      appId: '${options.appId}',
+      measurementId: '${options.measurementId}'
+    })`
+        }
+        recorder.insertLeft(change.pos, change.toAdd);
+      }
+    // }
+  });
+  host.commitUpdate(recorder);
+  return host
+}
+
