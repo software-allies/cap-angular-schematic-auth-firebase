efb15ff6fd757cbf767a8cd21c1a0ee0db4f0976
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-05 16:24:10 -0600

    CAP-342 update the file cap-auth.module.ts psra import AngularFireModule.initializeApp({}) from @angular/fire and AuthenticationModule from cap-authentication-firebase and clean authentication.service.ts file.

--- src/cap-angular-schematic-auth-firebase/files/cap-auth.module.ts
+++ src/cap-angular-schematic-auth-firebase/files/cap-auth.module.ts
@@ -1,18 +1,34 @@
 import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './cap-auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ForgotComponent } from './forgot/forgot.component';
 import { ProfileComponent } from './profile/profile.component';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AuthenticationService } from './authentication.service';
+
+import { AuthenticationModule } from 'cap-authentication-firebase';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AuthenticationModule,
+    AngularFireModule.initializeApp({
+      apiKey: '<%=apiKey%>',
+      authDomain: '<%=authDomain%>',
+      databaseURL: '<%=databaseURL%>',
+      projectId: '<%=projectId%>',
+      storageBucket: '<%=storageBucket%>',
+      messagingSenderId: '<%=senderId%>',
+      appId: '<%=appId%>',
+      measurementId: '<%=measurementId%>'
+    }),
   ],
   declarations: [
     RegisterComponent,
@@ -25,7 +41,8 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
     LoginComponent,
     ForgotComponent,
     ProfileComponent
-  ]
+  ],
+  providers: [AuthenticationService]
 })
 export class CapAuthModule {
 }
