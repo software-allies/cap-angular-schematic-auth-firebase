378043c080fd35e22b219c4ab0b36306d9409493
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-27 10:02:42 -0600

    Update cap-authentication-firebase version.

--- src/cap-angular-schematic-auth-firebase/index.ts
+++ src/cap-angular-schematic-auth-firebase/index.ts
@@ -81,7 +81,7 @@ export function capAngularSchematicAuthFirebase(_options: any): Rule {
 export function addPackageJsonDependencies(): Rule {
   return (host: Tree, context: SchematicContext) => {
     const dependencies: NodeDependency[] = [
-      { type: NodeDependencyType.Default, version: '^1.0.4', name: 'cap-authentication-firebase' },
+      { type: NodeDependencyType.Default, version: '^1.0.5', name: 'cap-authentication-firebase' },
       { type: NodeDependencyType.Default, version: '^7.2.3', name: 'firebase' },
       { type: NodeDependencyType.Default, version: '^5.2.1', name: '@angular/fire' },
       { type: NodeDependencyType.Default, version: '^4.3.1', name: 'bootstrap' },
